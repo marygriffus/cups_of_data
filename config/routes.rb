6704ad3f37e6d72053501fc54c6f36c29db32dd9
Mary Griffus <mary.e.shultz@gmail.com>
2016-04-28 17:40:55 -0400

    add about me page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/about'
+
   root 'bras#index'
   resources :bras do
     resources :bra_sizes, only: [:new, :create, :edit, :update, :destroy]
